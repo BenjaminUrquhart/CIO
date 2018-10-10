1f7a968f2f72e78f3cf3b8d8ad46788c4fd2e5bd
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-10-10 01:13:26 -0400

    here we go

---
+++ src/net/benjaminurquhart/CIS/ChannelInputStream.java
@@ -0,0 +1,40 @@
+package net.benjaminurquhart.CIS;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.PrivateChannel;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+public class ChannelInputStream extends InputStream {
+	
+	private Listener listener;
+	
+	public ChannelInputStream(TextChannel channel, boolean ignoreBots) {
+		this.listener = new Listener(channel, ignoreBots);
+	}
+	
+	public ChannelInputStream(MessageChannel channel, Guild guild, boolean ignoreBots) {
+		this.listener = new Listener(channel, guild, ignoreBots);
+	}
+	
+	public ChannelInputStream(PrivateChannel channel, boolean ignoreBots) {
+		this.listener = new Listener(channel, ignoreBots);
+	}
+	@Override
+	public int read() throws IOException {
+		try {
+			return listener.getNextChar();
+		}
+		catch(Exception e) {
+			return -1;
+		}
+	}
+	@Override
+	public void close() throws IOException {
+		listener.close();
+		super.close();
+	}
+}
