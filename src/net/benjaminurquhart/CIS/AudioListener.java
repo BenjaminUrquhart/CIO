0246469d3e10953e266147e07209026ac09aed0f
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-10-10 15:40:39 -0400

    Added VoiceChannel input stream support

--- src/net/benjaminurquhart/CIS/AudioListener.java
+++ src/net/benjaminurquhart/CIS/AudioListener.java
@@ -1,15 +1,25 @@
 package net.benjaminurquhart.CIS;
 
+import java.util.ArrayList;
+
 import net.dv8tion.jda.core.audio.AudioReceiveHandler;
 import net.dv8tion.jda.core.audio.CombinedAudio;
 import net.dv8tion.jda.core.audio.UserAudio;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 
 public class AudioListener implements AudioReceiveHandler, Listener{
 	
+	private Guild guild;
+	private ArrayList<Byte> buff;
 	
 	public AudioListener(VoiceChannel channel) {
-		
+		this.guild = channel.getGuild();
+		if(guild.getAudioManager().isConnected()) {
+			throw new IllegalStateException("Already receiving audio from this guild!");
+		}
+		guild.getAudioManager().setReceivingHandler(this);
+		guild.getAudioManager().openAudioConnection(channel);
 	}
 	@Override
 	public boolean canReceiveCombined() {
@@ -23,27 +33,30 @@ public class AudioListener implements AudioReceiveHandler, Listener{
 
 	@Override
 	public void handleCombinedAudio(CombinedAudio arg0) {
-		
+		byte[] arr = arg0.getAudioData(1.0);
+		for(byte b : arr) {
+			buff.add(b);
+		}
 	}
 
 	@Override
 	public void handleUserAudio(UserAudio arg0) {
-		
+		return;
 	}
 
 	@Override
 	public int getNext() {
-		return 0;
+		return buff.remove(0);
 	}
 
 	@Override
 	public int available() {
-		return 0;
+		return buff.size();
 	}
 
 	@Override
 	public void close() {
-		
+		guild.getAudioManager().closeAudioConnection();
 	}
 
 }
